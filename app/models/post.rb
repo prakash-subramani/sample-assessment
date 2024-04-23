40f45ec77a4cb25dfd0e0787551aa06e2ee7053b
Prakash Subramani <prakash@mallow-tech.com>
2024-04-23 17:22:24 +0530

    Posting to slack channel after creating message. This will post message into specific channel.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,11 @@ class Post < ApplicationRecord
   has_many :taggings, as: :taggable
   has_many :tags, through: :taggings
   accepts_nested_attributes_for :tags
+
+  after_create_commit :post_to_slack
+
+  def post_to_slack
+    SlackService.new({text: self.content}).post_message
+  end
+
 end
